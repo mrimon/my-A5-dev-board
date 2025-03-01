bd87c2804300c9a0547fa0adb79a9bb99451469b
mrimon <rmamun1585@gmail.com>
2025-03-01 12:16:44 +0600

    html codes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//  /** @type {import('tailwindcss').Config} */
+//  export default {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//       extend: {},
+//       colors: {
+//         "primary-bg": "#1E1E1E",
+//         "title-color": "#00303C",
+//         "accent-text": "#3752FD"
+//       }
+//     },
+//     plugins: [],
+//   }
